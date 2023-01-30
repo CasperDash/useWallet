277799cf5963f6eb251719f126ae55ea1765afe4
Liam <20489407+hoailinh1210@users.noreply.github.com>
2023-01-30 23:02:21 +0700

    feat: sign example with react (#12)

--- examples/example-react/src/App.tsx
+++ examples/example-react/src/App.tsx
@@ -3,6 +3,7 @@ import { useAccount, useDisconnect } from '@usedapp/react';
 import './App.css';
 import CasperDashButton from './component/CasperDashButton';
 import CasperSignerButton from './component/CasperSignerButton';
+import FormSigner from './component/FormSigner';
 
 function App() {
   const { publicKey } = useAccount();
@@ -10,27 +11,32 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://casperdash.io" target="_blank">
-          <img src="/casperdash.png" className="logo" alt="CasperDash logo" />
-        </a>
-      </div>
-      <h1>UseDApp Connector</h1>
-      <div className="card">
-        {
-          publicKey ? (
-            <button onClick={() => disconnect()}>
-              Disconnect {publicKey}
-            </button>
-          ) : (
-            <>
-              <CasperSignerButton/>
-              <br/>
-              <CasperDashButton/>
-            </>
-          )
-        }
-      </div>
+        {publicKey ? (
+          <div>
+            <button style={{ marginTop: '-40px' }} onClick={() => disconnect()}>Disconnect {publicKey}</button>
+            <div className="signer-form-wrapper">
+              <FormSigner />
+            </div>
+          </div>
+        ) : (
+          <>
+            <div>
+              <a href="https://casperdash.io" target="_blank">
+                <img
+                  src="/casperdash.png"
+                  className="logo"
+                  alt="CasperDash logo"
+                />
+              </a>
+            </div>
+            <div className="card">
+              <h1>UseDApp Connector</h1>
+              <CasperSignerButton />
+              <br />
+              <CasperDashButton />
+            </div>
+          </>
+        )}
     </div>
   );
 }
--- packages/react/src/hooks/useSignMessage.ts
+++ packages/react/src/hooks/useSignMessage.ts
@@ -3,24 +3,27 @@ import { useMutation, UseMutationOptions } from '@tanstack/react-query';
 import {
   signMessage as signMessageDapp,
   SignMessageParams,
-  SignResult,
+  SignMessageResult,
 } from '@usedapp/core';
 import { MutationKeysEnum } from '@usedapp/react';
 
-export type UseSignMessageParams = SignMessageParams;
+export type UseSignMessageParams = Partial<SignMessageParams>;
 
 export type UseSignMessageConfig = Partial<
-UseMutationOptions<SignResult, unknown, UseSignMessageParams>
+UseMutationOptions<SignMessageResult, unknown, UseSignMessageParams>
 >;
 
-export type UseSignMessageProps = UseSignMessageParams & UseSignMessageConfig;
+export type UseSignMessageProps = Partial<UseSignMessageParams & UseSignMessageConfig>;
 
-const mutationFn = async (args: UseSignMessageParams) => {
-  const { message } = args;
+const mutationFn = async ({ message, signingPublicKey }: UseSignMessageParams) => {
   if (!message) {
     throw new Error('Message must be a non-empty string');
   }
-  return signMessageDapp(args);
+  if (!signingPublicKey) {
+    throw new Error('signingPublicKey must be a non-empty string');
+  }
+
+  return signMessageDapp({ message, signingPublicKey });
 };
 
 export const useSignMessage = ({
@@ -30,7 +33,7 @@ export const useSignMessage = ({
   onMutate,
   onSettled,
   onSuccess,
-}: UseSignMessageProps) => {
+}: UseSignMessageProps = {}) => {
   const {
     data,
     error,
@@ -43,26 +46,26 @@ export const useSignMessage = ({
     variables,
     mutate,
     mutateAsync,
-  } = useMutation([MutationKeysEnum.SIGN_MESSAGE], mutationFn, {
+  } = useMutation([MutationKeysEnum.SIGN_MESSAGE, signingPublicKey, message], mutationFn, {
     onError,
     onMutate,
     onSettled,
     onSuccess,
   });
 
-  const signMessage = useCallback(() => {
-    return mutate({
+  const signMessage = useCallback((params: UseSignMessageParams) => {
+    return mutate(params || {
       message,
       signingPublicKey,
     });
-  }, [message, signingPublicKey]);
+  }, [message, signingPublicKey, mutate]);
 
-  const signMessageAsync = useCallback(async () => {
-    return mutateAsync({
+  const signMessageAsync = useCallback(async (params: UseSignMessageParams) => {
+    return mutateAsync(params || {
       message,
       signingPublicKey,
     });
-  }, [message, signingPublicKey]);
+  }, [message, signingPublicKey, mutateAsync]);
 
   return {
     signMessage,
