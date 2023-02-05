08a073fc806dc6dac561186713b61cd4be016459
Liam <hoailinh1210@gmail.com>
2023-02-06 00:25:42 +0700

    test(react): connect and disconnect

---
+++ packages/react/test/reactUtil.tsx
@@ -0,0 +1,29 @@
+import { QueryClient } from '@tanstack/react-query';
+import { CasperDashConnector } from '@usedapp/core';
+import { CasperProvider, Client, createClient } from '@usedapp/react';
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: Infinity,
+      retry: false,
+    },
+  },
+  logger: {
+    error: () => {},
+    log: console.log,
+    warn: console.warn,
+  },
+});
+
+type Props = { client?: Client } & {
+  children?:
+  | React.ReactElement
+  | React.ReactNode;
+};
+export function wrapper({
+  client = createClient({ connectors: [new CasperDashConnector()] }),
+  ...rest
+}: Props = {}) {
+  return <CasperProvider client={client} {...rest} />;
+}
