d58613e85db44e11d3542ecfe1fe19194388f5d0
Liam <20489407+hoailinh1210@users.noreply.github.com>
2023-02-08 01:53:51 +0700

    docs: init source base (#21)

---
+++ docs/theme.config.tsx
@@ -0,0 +1,17 @@
+import { DocsThemeConfig } from 'nextra-theme-docs';
+
+const config: DocsThemeConfig = {
+  logo: <span>UseDApp - Casper</span>,
+  project: {
+    link: 'https://casperdash.io/',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/CasperDash/useDApp',
+  footer: {
+    text: 'useDApp Casper Docs',
+  },
+};
+
+export default config;
