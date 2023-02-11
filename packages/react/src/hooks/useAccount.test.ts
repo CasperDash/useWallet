52b01f57c786c8f909b3463ebd2e7d20333e16f4
Liam <20489407+hoailinh1210@users.noreply.github.com>
2023-02-11 16:11:29 +0700

    test: useAccount hook (#28)

---
+++ packages/react/src/hooks/useAccount.test.ts
@@ -0,0 +1,34 @@
+import { renderHook } from '@testing-library/react';
+import { getAccount, StatusEnum } from '@usedapp/core';
+import { beforeEach, describe, expect, it, MockedFunction, vi } from 'vitest';
+
+import { useAccount } from './useAccount';
+
+vi.mock('@usedapp/core', () => ({
+  getAccount: vi.fn(),
+  watchAccount: vi.fn(),
+  StatusEnum: {
+    DISCONNECTED: 'DISCONNECTED',
+    CONNECTED: 'CONNECTED',
+  },
+}));
+
+describe('useAccount', () => {
+  const getAccountMock = getAccount as MockedFunction<typeof getAccount>;
+
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should set initial values correctly when account is connected', () => {
+    const publicKey = 'some_public_key';
+    const status = StatusEnum.CONNECTED;
+    getAccountMock.mockImplementationOnce(() => ({ publicKey, status }));
+
+    const { result } = renderHook(() => useAccount());
+    expect(result.current).toEqual({
+      publicKey,
+      status,
+    });
+  });
+});
