4ccde246a43a8d00e386d5228a5133fe6d66121d
Liam <hoailinh1210@gmail.com>
2023-02-16 15:41:15 +0700

    docs: update meta and onError

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -5,12 +5,18 @@ const config: DocsThemeConfig = {
   project: {
     link: 'https://casperdash.io/',
   },
+  darkMode: true,
   chat: {
     link: 'https://discord.com',
   },
   docsRepositoryBase: 'https://github.com/CasperDash/useDApp',
   footer: {
     text: 'Built by CasperDash team',
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – useWallet',
+    };
   },
 };
 
