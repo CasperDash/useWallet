28060838045b2d6f6a5a695c2e5a760855ce3d05
Liam <hoailinh1210@gmail.com>
2023-02-24 00:37:59 +0700

    fix: onConnect event with useAccount

--- packages/core/src/utils/deepEqual.ts
+++ packages/core/src/utils/deepEqual.ts
@@ -1,23 +1,42 @@
-export const deepEqual = (a: any, b: any) => {
-  // check if a and b are both null or undefined
-  if (a === null && b === null) return true;
-  if (a === undefined && b === undefined) return true;
+/** Forked from https://github.com/epoberezkin/fast-deep-equal */
 
-  // check if a and b are of the same type
-  if (typeof a !== typeof b) return false;
+export function deepEqual(a: any, b: any) {
+  if (a === b) return true;
 
-  // check if a and b are both primitive types
-  if (typeof a !== 'object') return a === b;
+  if (a && b && typeof a === 'object' && typeof b === 'object') {
+    if (a.constructor !== b.constructor) return false;
 
-  // check if a and b have the same number of properties
-  if (Object.keys(a as Record<any, any>).length !== Object.keys(b as Record<any, any>).length) return false;
+    let length: number;
+    let i: number;
 
-  // check if all properties of a exist in b and are equal
-  // eslint-disable-next-line no-restricted-syntax
-  for (const key in a) {
-    if (!Object.prototype.hasOwnProperty.call(b, key)) return false;
-    if (!deepEqual(a[key], b[key])) return false;
+    if (Array.isArray(a) && Array.isArray(b)) {
+      length = a.length;
+      if (length != b.length) return false;
+      for (i = length; i-- !== 0; ) if (!deepEqual(a[i], b[i])) return false;
+      return true;
+    }
+
+    if (a.valueOf !== Object.prototype.valueOf)
+      return a.valueOf() === b.valueOf();
+    if (a.toString !== Object.prototype.toString)
+      return a.toString() === b.toString();
+
+    const keys = Object.keys(a);
+    length = keys.length;
+    if (length !== Object.keys(b).length) return false;
+
+    for (i = length; i-- !== 0; )
+      if (!Object.prototype.hasOwnProperty.call(b, keys[i]!)) return false;
+
+    for (i = length; i-- !== 0; ) {
+      const key = keys[i];
+
+      if (key && !deepEqual(a[key], b[key])) return false;
+    }
+
+    return true;
   }
 
-  return true;
-};
+  // true if both NaN, false otherwise
+  return a !== a && b !== b;
+}
