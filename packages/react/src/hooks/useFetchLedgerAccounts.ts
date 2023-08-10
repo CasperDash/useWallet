4facb2ff24849d80fbd721ca40d49a4e02931479
Liam <hoailinh1210@gmail.com>
2023-08-11 01:04:24 +0700

    feat: infinities scroll for fetch ledger

--- packages/react/src/hooks/useFetchLedgerAccounts.ts
+++ packages/react/src/hooks/useFetchLedgerAccounts.ts
@@ -1,29 +1,56 @@
+/* eslint-disable @typescript-eslint/indent */
 import { getLedgerPath, getLedgerPublicKey } from '@casperdash/usewallet-core';
-import { useQuery, UseQueryOptions } from '@tanstack/react-query';
+import { QueryFunctionContext, useInfiniteQuery, UseInfiniteQueryOptions } from '@tanstack/react-query';
 
-type Params = { total: number };
+type Params = {
+  startIndex?: number;
+  total?: number;
+};
 
 export type LedgerAccount = {
   path: string;
   publicKey?: string;
-  index: string;
+  index: number;
 };
 
+type Options =
+UseInfiniteQueryOptions<LedgerAccount[], unknown, LedgerAccount[]>;
+
 export const useFetchLedgerAccounts = (
-  { total }: Params,
-  options?: UseQueryOptions<LedgerAccount[], unknown, LedgerAccount[], [string]>) => {
-  return useQuery(['ledger-accounts'], async () => {
-    const accounts = [];
-    for (let i = 0; i < total; i++) {
-      const publicKey = await getLedgerPublicKey({ index: i.toString() });
+  { startIndex = 0, total = 10 }: Params,
+  options?: Options,
+  ) => {
+  const { data, ...query } = useInfiniteQuery<LedgerAccount[]>(
+    ['casper-ledger-accounts'],
+    async (context: QueryFunctionContext) => {
+      const accounts = [];
+      const { pageParam = startIndex } = context;
+      console.log('passgeParam: ', context);
+      for (let i = pageParam; i < pageParam + total; i++) {
+        const publicKey = await getLedgerPublicKey({ index: i.toString() });
 
-      accounts.push({
-        path: getLedgerPath(i.toString()),
-        publicKey,
-        index: i.toString(),
-      });
-    }
+        accounts.push({
+          path: getLedgerPath(i.toString()),
+          publicKey,
+          index: i,
+        });
+      }
 
-    return accounts;
-  }, options);
+      return accounts;
+    },
+    {
+      ...options,
+      getNextPageParam: (lastPage: LedgerAccount[]) => (lastPage[lastPage.length - 1]?.index || 0) + 1,
+    },
+  );
+
+  const flattedData = data?.pages.reduce((acc: LedgerAccount[], page: LedgerAccount[]) => {
+    return [...acc, ...page];
+  }, [] as LedgerAccount[]);
+
+  return {
+    data: flattedData,
+    paggedData: data,
+    ...query,
+  };
 };
